6f07138a389cbe09b5b8264d6c8609aa380f6af0
andreucastano01 <andreu.castano01@estudiant.upf.edu>
2022-04-21 16:17:56 +0200

    Hasta el paso 5 completo

--- P1/Assets/Scripts/PlayerController.cs
+++ P1/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-    private rigidbody rb;
+    private Rigidbody rb;
     private float movementX;
     private float movementY;
 
@@ -16,15 +16,14 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
